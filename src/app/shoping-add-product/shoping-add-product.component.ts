6c25bf68464b5859e52d1a84e35eff4a912d71a6
jaseel <jaseeljctcs@gmail.com>
2022-11-28 16:16:58 +0530

    add read data

--- src/app/shoping-add-product/shoping-add-product.component.ts
+++ src/app/shoping-add-product/shoping-add-product.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./shoping-add-product.component.css']
 })
 export class ShopingAddProductComponent {
+ 
 
+  pimage=""
+  disc=""
+  price=""
+  sname=""
+  q=""
+  view=()=>{
+    let data:any={
+      'pimage':this.pimage,
+      'disc':this.disc,
+      'price':this.price,
+      'sname':this.sname,
+      'q':this.q
+    }
+    console.log(data)
+  }
 }
